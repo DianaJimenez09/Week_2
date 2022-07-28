e9302098d54192ab7c126e4d9d0275e7e581c944
DianaJimenez09 <djimenezneculman@gmail.com>
2022-07-28 14:50:56 +0200

    person

---
+++ POO/person.js
@@ -0,0 +1,16 @@
+class Person {
+
+    constructor(coloPelo, colorOjos, nacionalidad){
+        this.coloPelo = coloPelo;
+        this.colorOjos = colorOjos;
+        this.altura = altura;
+        this.genero = genero;
+        this.edad = edad;
+        this.nacionalidad = nacionalidad;
+
+    }
+}
+
+let PersonA = new Person ("castaño","marrones","española")
+
+console.log(PersonA)
